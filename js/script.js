56484267185927a1b9ef758ce296db9a92e77ca7
CoolerProYT <veronlam1818@gmail.com>
2024-08-20 09:45:29 +0800

    update navigation bar

--- js/script.js
+++ js/script.js
@@ -2,8 +2,6 @@ $("#nav").load("shared/nav_bar.html");
 
 function loadPage(filePath, id){
     $("#content").load(filePath);
-    $(".nav-link").removeClass('active');
-    $("#" + id).addClass('active');
 }
 
 function loadImage(fileName, description){
