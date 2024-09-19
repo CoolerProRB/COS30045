0ce4987a458f4e4d7b4c2f257a6dedb5204f2976
CoolerProYT <veronlam1818@gmail.com>
2024-09-19 12:29:18 +0800

    lab 3 demo

--- js/lab5.3.js
+++ js/lab5.3.js
@@ -3,13 +3,13 @@ height = 300;
 
 padding = 20;
 
-transition = d3.easeCubicInOut;
-
 dataSet = [14, 5, 26, 23, 9, 21, 7, 19, 22, 16, 2, 10];
 
+// Scales
 xScale = d3.scaleBand().domain(d3.range(dataSet.length)).rangeRound([padding, width - padding]).paddingInner(0.05);
 yScale = d3.scaleLinear().domain([d3.max(dataSet), 0]).rangeRound([padding, height]);
 
+// Axes
 xAxis = d3.axisBottom()
     .scale(xScale)
     .ticks(dataSet.length);
@@ -18,11 +18,13 @@ yAxis = d3.axisLeft()
     .scale(yScale)
     .ticks(10);
 
+// SVG
 svg = d3.select("#container")
     .append("svg")
     .attr("width", width)
     .attr("height", height + padding);
 
+// Bars
 svg.selectAll("rect")
     .data(dataSet)
     .enter()
@@ -39,6 +41,7 @@ svg.selectAll("rect")
     })
     .style("fill", "slategray")
 
+// Axes
 svg.append("g")
     .attr("class", "x-axis")
     .attr("transform", "translate(0," + (height) + ")")
@@ -48,6 +51,7 @@ svg.append("g")
     .attr("transform", "translate(" + padding + ",0)")
     .call(yAxis);
 
+// Buttons for adding data
 d3.select(".btn-l-5-1")
     .on("click", function () {
         let maxValue = 25;
@@ -85,6 +89,7 @@ d3.select(".btn-l-5-1")
             .call(xAxis);
     });
 
+// Buttons for removing data
 d3.select(".btn-l-5-2")
     .on("click", function () {
         dataSet.pop();
